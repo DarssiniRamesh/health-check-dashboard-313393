e47e9f97a940edaeb759e6a483263923819e3810
Kavia AI <production@kavia.ai>
2026-02-03 07:12:20 +0000

    Checkpoint 5 - cmef60cae1

--- backend_api/src/test/java/com/example/backendapi/OpenApiDocsSmokeTest.java
+++ backend_api/src/test/java/com/example/backendapi/OpenApiDocsSmokeTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.http.ResponseEntity;
  * connection: the goal is to ensure OpenAPI generation works even when DB config is absent.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = backendapiApplication.class)
+@Import(test_JdbcTemplateTestConfig.class)
 class OpenApiDocsSmokeTest {
 
     @Autowired private TestRestTemplate restTemplate;
--- backend_api/src/test/java/com/example/backendapi/test_BackendHealthFullStackTest.java
+++ backend_api/src/test/java/com/example/backendapi/test_BackendHealthFullStackTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -23,6 +24,7 @@ import org.springframework.http.ResponseEntity;
             "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration",
             "spring.sql.init.mode=never"
         })
+@Import(test_JdbcTemplateTestConfig.class)
 class test_BackendHealthFullStackTest {
 
     @Autowired private TestRestTemplate restTemplate;
